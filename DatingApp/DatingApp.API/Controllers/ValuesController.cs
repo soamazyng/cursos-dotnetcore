a41efd1513c3262d0c638b10604a202febbb3924
Jaqueline Benedicto <soamazing@gmail.com>
2018-09-05 18:26:48 -0300

    curso

--- DatingApp/DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp/DatingApp.API/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
@@ -10,11 +11,21 @@ namespace DatingApp.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        //inicia o context
+        private readonly DataContext _context;
+
+        //injetando a dependência
+        public ValuesController(DataContext context)
+        {            
+            _context = context;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult GetValues()
         {
-            return new string[] { "value1", "value2", "value2" };
+            var values = _context.Values.ToList();
+            return Ok(values);
         }
 
         // GET api/values/5
