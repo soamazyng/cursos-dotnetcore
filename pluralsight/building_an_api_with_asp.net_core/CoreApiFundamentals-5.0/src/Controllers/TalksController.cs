4ef2da57e88b19cd7e79379ef6fc4e3ddba05360
Jay Benedicto <soamazing@gmail.com>
2021-04-13 17:08:42 -0300

    +

---
+++ pluralsight/building_an_api_with_asp.net_core/CoreApiFundamentals-5.0/src/Controllers/TalksController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [ApiController]
+    public class TalksController : ControllerBase
+    {
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
+
+        public TalksController(ICampRepository repository, IMapper mapper, LinkGenerator linkGenerator)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _linkGenerator = linkGenerator;
+        }
+    }
+}
